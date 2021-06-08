8e481ff9f8a599192dac6098d3a309fe4edb4dd7
YashiroSennzai <61809595+YashiroSennzai@users.noreply.github.com>
2021-06-07 21:28:55 -0700

    Updates

---
+++ precache-manifest.d914263f32b0c9520e6881dc42589731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb66a78bce544f3c27bc6673a05a599",
+    "url": "/Inspected-Job-App/index.html"
+  },
+  {
+    "revision": "e764d03d63c4992ef100",
+    "url": "/Inspected-Job-App/static/css/main.a9a5e568.chunk.css"
+  },
+  {
+    "revision": "46b46496d5f2be9e8ab4",
+    "url": "/Inspected-Job-App/static/js/2.c0de714a.chunk.js"
+  },
+  {
+    "revision": "e764d03d63c4992ef100",
+    "url": "/Inspected-Job-App/static/js/main.e812ecce.chunk.js"
+  },
+  {
+    "revision": "b9ddfed98b49348f2413",
+    "url": "/Inspected-Job-App/static/js/runtime~main.21eeed32.js"
+  }
+]);
